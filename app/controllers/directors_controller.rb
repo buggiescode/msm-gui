b383cc126fcc7558ed4aee474467252520e53903
buggiescode <137247769+buggiescode@users.noreply.github.com>
2025-05-21 07:55:51 +0000

    added CRUD for Directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -36,4 +36,46 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/eldest" })
   end
+
+  def create
+    name = params.fetch("name")
+    dob = params.fetch("dob")
+    bio = params.fetch("bio")
+    image = params.fetch("image")
+
+    director = Director.new
+    director.name = name
+    director.dob = dob
+    director.bio = bio
+    director.image = image
+
+    director.save
+
+    redirect_to("/directors")
+  end
+
+  def destroy
+    director_id = params.fetch("director_id")
+
+    director_to_delete = Director.where({ :id => director_id }).first
+
+    director_to_delete.destroy
+
+    redirect_to("/directors")
+  end
+
+  def update
+    director_id = params.fetch("director_id")
+
+    the_director = Director.where({ :id => director_id }).first
+
+    the_director.name = params.fetch("name")
+    the_director.dob = params.fetch("dob")
+    the_director.bio = params.fetch("bio")
+    the_director.image = params.fetch("image")
+
+    the_director.save
+
+    redirect_to("/directors/#{the_director.id}")
+  end
 end
