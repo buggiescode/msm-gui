6491527e6e517efe4eca80d41f177454c09e8f00
buggiescode <137247769+buggiescode@users.noreply.github.com>
2025-05-21 07:34:07 +0000

    Finished Actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,4 +14,46 @@ class ActorsController < ApplicationController
       
     render({ :template => "actor_templates/show" })
   end
+
+  def create
+    name = params.fetch("name")
+    dob = params.fetch("dob")
+    bio = params.fetch("bio")
+    image = params.fetch("image")
+
+    actor = Actor.new
+    actor.name = name
+    actor.dob = dob
+    actor.bio = bio
+    actor.image = image
+
+    actor.save
+
+    redirect_to("/actors")
+  end
+
+  def destroy
+    actor_id = params.fetch("actor_id")
+
+    actor_to_delete = Actor.where({ :id => actor_id }).first
+
+    actor_to_delete.destroy
+
+    redirect_to("/actors")
+  end
+
+  def update
+    actor_id = params.fetch("actor_id")
+
+    the_actor = Actor.where({ :id => actor_id }).first
+
+    the_actor.name = params.fetch("name")
+    the_actor.dob = params.fetch("dob")
+    the_actor.bio = params.fetch("bio")
+    the_actor.image = params.fetch("image")
+
+    the_actor.save
+
+    redirect_to("/actors/#{the_actor.id}")
+  end
 end
